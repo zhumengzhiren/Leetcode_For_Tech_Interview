3d1fab7b5d6ec12a5263d7a6fdbe2fc1475ab79b
zhumengzhiren <yuanfan0504@gmail.com>
2025-03-06 22:12:04 +0800

    5

--- 二叉树层序遍历/二叉树的层平均值.py
+++ 二叉树层序遍历/二叉树的层平均值.py
@@ -11,22 +11,27 @@
 # 输入：root = [3,9,20,15,7]
 # 输出：[3.00000,14.50000,11.00000]
 
+# 层序遍历五部曲
 def averageOfLevels(self, root):
+    # 创建结果集
     results = []
     if not root:
         results
+    # 创建队列
     q = [root]
     while q:
+        # 进行预保存队列长度和返回总和
         size = len(q)
         sum = 0
         for _ in range(size):
+            # 在队列中加入子树
             node = q.pop(0)
             sum += node.val
             if node.left:
                 q.append(node.left)
             if node.right:
                 q.append(node.right)
-            
+        # 加入结果
         results.append(sum/size)
-
+    # 返回结果
     return results
